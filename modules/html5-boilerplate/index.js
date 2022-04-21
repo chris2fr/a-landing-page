d433be4a3b7e54a6716f5cbe4ce1009e8c92c445
chris2fr <chris@mann.fr>
2022-04-21 08:56:24 +0200

    Added Page Content

--- modules/html5-boilerplate/index.js
+++ modules/html5-boilerplate/index.js
@@ -1,6 +1,26 @@
 module.exports = {
   extend: '@apostrophecms/page-type',
   options: {
-    label: 'HTML5 Boilerplate'
+    label: 'HTML5 Boilerplate',
+    pluralLable: 'HTML5 BOILERPLATES'
+  },
+  fields: {
+    add: {
+      main: {
+        type: 'area',
+        options: {
+          widgets: require('../../lib/area').fullConfig
+        }
+      }
+    },
+    group: {
+      basics: {
+        label: 'Basics',
+        fields: [
+          'title',
+          'main'
+        ]
+      }
+    }
   }
 };
