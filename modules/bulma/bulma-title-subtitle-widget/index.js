2a70dc265a05418506e68625d6a3c85ce47e09b5
Chris Mann <chris@mann.fr>
2022-04-22 15:26:32 +0200

    Looks good for web alpha

--- modules/bulma/bulma-title-subtitle-widget/index.js
+++ modules/bulma/bulma-title-subtitle-widget/index.js
@@ -1,52 +1,42 @@
 module.exports = {
-  extend: '@apostrophecms/widget-type',
+  extend: '@apostrophecms/rich-text-widget',
   options: {
-    label: 'Bulma Title & Subtitle'
-  },
-  fields: {
-    add: {
-      titleSubtitle: {
-        type: 'area',
-        lable: 'Title and Subtitle',
-        options: {
-          widgets: {
-            '@apostrophecms/rich-text': {
-              class: 'container',
-              toolbar: [
-                'styles',
-                'bold',
-                'italic',
-                'alignLeft',
-                'alignCenter',
-                'alignRight'
-              ],
-              styles: [
-                // you may also use a `class` property with these
-                {
-                  tag: 'h2',
-                  label: 'Heading 2 (H2)',
-                  class: 'title'
-                },
-                {
-                  tag: 'h3',
-                  label: 'Heading 3 (H3)',
-                  class: 'title'
-                },
-                {
-                  tag: 'h4',
-                  label: 'Heading 4 (H4)',
-                  class: 'title'
-                },
-                {
-                  tag: 'p',
-                  label: 'Subtitle',
-                  class: 'subtitle'
-                }    
-              ]
-            }
-          }
-        }
+    name: 'bulma-title-subtitle',
+    label: 'Title with Subtitle',
+    toolbar: [
+      'styles',
+      'bold',
+      'italic',
+      'strike',
+      'alignLeft',
+      'alignCenter',
+      'alignRight'
+    ],
+    styles: [
+      // you may also use a `class` property with these
+      {
+        tag: 'h2',
+        label: 'Heading 2 (H2)',
+        class: 'title'
+
+      },
+      {
+        tag: 'h3',
+        label: 'Heading 3 (H3)',
+        class: 'title'
+
+      },
+      {
+        tag: 'h4',
+        label: 'Heading 4 (H4)',
+        class: 'title'
+
+      },
+      {
+        tag: 'p',
+        label: 'SubHeading Paragraph (P)',
+        class: 'subtitle'
       }
-    }
+    ]
   }
 };
