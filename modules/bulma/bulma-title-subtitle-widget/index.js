8fb3903803738d8e09c60f466aad554f11433ac2
chris2fr <chris@mann.fr>
2022-04-22 09:48:51 +0200

    Integration of Bulma

--- modules/bulma/bulma-title-subtitle-widget/index.js
+++ modules/bulma/bulma-title-subtitle-widget/index.js
@@ -7,40 +7,43 @@ module.exports = {
     add: {
       titleSubtitle: {
         type: 'area',
+        lable: 'Title and Subtitle',
         options: {
-          '@apostrophecms/rich-text': {
-            class: 'container',
-            toolbar: [
-              'styles',
-              'bold',
-              'italic',
-              'alignLeft',
-              'alignCenter',
-              'alignRight'
-            ],
-            styles: [
-              // you may also use a `class` property with these
-              {
-                tag: 'h2',
-                label: 'Heading 2 (H2)',
-                class: 'title'
-              },
-              {
-                tag: 'h3',
-                label: 'Heading 3 (H3)',
-                class: 'title'
-              },
-              {
-                tag: 'h4',
-                label: 'Heading 4 (H4)',
-                class: 'title'
-              },
-              {
-                tag: 'p',
-                label: 'Subtitle',
-                class: 'subtitle'
-              }    
-            ]
+          widgets: {
+            '@apostrophecms/rich-text': {
+              class: 'container',
+              toolbar: [
+                'styles',
+                'bold',
+                'italic',
+                'alignLeft',
+                'alignCenter',
+                'alignRight'
+              ],
+              styles: [
+                // you may also use a `class` property with these
+                {
+                  tag: 'h2',
+                  label: 'Heading 2 (H2)',
+                  class: 'title'
+                },
+                {
+                  tag: 'h3',
+                  label: 'Heading 3 (H3)',
+                  class: 'title'
+                },
+                {
+                  tag: 'h4',
+                  label: 'Heading 4 (H4)',
+                  class: 'title'
+                },
+                {
+                  tag: 'p',
+                  label: 'Subtitle',
+                  class: 'subtitle'
+                }    
+              ]
+            }
           }
         }
       }
